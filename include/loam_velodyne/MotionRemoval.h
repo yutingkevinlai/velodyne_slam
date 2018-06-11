8af0d078313ab9ed98bf1d33cba871f239b031b6
Kevin83825 <kevin83825.ece05g@g2.nctu.edu.tw>
2018-06-11 21:22:14 +0800

    fix publish topic

--- include/loam_velodyne/MotionRemoval.h
+++ include/loam_velodyne/MotionRemoval.h
@@ -1,11 +1,26 @@
 #ifndef LOAM_MOTIONREMOVAL_H
 #define LOAM_MOTIONREMOVAL_H
 
+#include <iostream>
+#include <vector>
 #include "common.h"
 #include <ros/node_handle.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/min_cut_segmentation.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/visualization/cloud_viewer.h>
 #include <tf/transform_broadcaster.h>
 
 namespace loam {
@@ -16,7 +31,7 @@ namespace loam {
 class MotionRemoval {
 public:
   MotionRemoval();
-		~MotionRemoval();
+  ~MotionRemoval();
 
   /** \brief Setup component.
    *
@@ -28,7 +43,9 @@ public:
 
   void prevCloudHandler(const sensor_msgs::PointCloud2ConstPtr& prevCloudMsg);
 
-		void curCloudHandler(const sensor_msgs::PointCloud2ConstPtr& curCloudMsg);
+  void curCloudHandler(const sensor_msgs::PointCloud2ConstPtr& curCloudMsg);
+
+  void segmentation(const pcl::PointCloud<pcl::PointXYZ>& cloudIn);
 
   /** \brief Handler method for mapping odometry messages.
    *
@@ -44,16 +61,16 @@ protected:
 */
 
 private:
-
-		pcl::PointCloud<pcl::PointXYZ> _prevCloud;
+  
+  pcl::PointCloud<pcl::PointXYZ> _prevCloud;
   pcl::PointCloud<pcl::PointXYZ> _curCloud; 
 
 
   ros::Time _timePrevCloud;      ///< time for previous cloud (cloud 3)
-		ros::Time _timeCurCloud;       ///< time for current cloud (cloud 2)
+  ros::Time _timeCurCloud;       ///< time for current cloud (cloud 2)
 
-	 ros::Publisher _pubTest;  ///< 
-
+  ros::Publisher _pubPrevCloud;  ///< 
+  ros::Publisher _pubCurCloud;
 
   ros::Subscriber _subPrevCloud;    ///< previous cloud subscriber
   ros::Subscriber _subCurCloud;    ///< current cloud subscriber
